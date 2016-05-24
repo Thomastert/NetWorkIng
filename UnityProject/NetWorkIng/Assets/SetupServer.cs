361663d8d63700ec1a54d71732710a9d2221f5d1
Thomacia <thomas@thoemda.com>
2016-05-24 16:04:25 +0200

    Start Project :SteamSalty

---
+++ UnityProject/NetWorkIng/Assets/SetupServer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupServer : MonoBehaviour {
+private const string typeName = "TankMoba";
+private const string gameName = "Room1";
+	
+    // Use this for initialization
+	private void StartServer()
+    {
+        Network.InitializeServer(2, 25000, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(typeName, gameName);
+
+	}
+
+    void OnServerInitialized()
+    {
+        Debug.Log("Server Initializied");
+    }
+
+    // Update is called once per frame
+    void Start () {
+        OnGUI();
+	}
+
+    void OnGUI()
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                StartServer();
+        }
+    }
+}
