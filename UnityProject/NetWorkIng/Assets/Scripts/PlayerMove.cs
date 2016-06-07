2e9b67f21ddb0dec08f1ea9873be3171e91fb2b1
Thomacia <thomas@thoemda.com>
2016-06-07 11:46:51 +0200

    more movement

--- UnityProject/NetWorkIng/Assets/Scripts/PlayerMove.cs
+++ UnityProject/NetWorkIng/Assets/Scripts/PlayerMove.cs
@@ -4,61 +4,29 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour
 {
-    [SerializeField]
-    private GameObject bulletPrefab;
-    [SerializeField]
-    private GameObject Target;
 
-    public override void OnStartLocalPlayer()
-    {
-        GetComponent<MeshRenderer>().material.color = Color.red;
-    }
-
-    [Command]
-    void CmdFire()
-    {
-        // This [Command] code is run on the server!
-
-        // create the bullet object locally
-        var bullet = (GameObject)Instantiate(
-             bulletPrefab,
-             transform.position - transform.forward,
-             Quaternion.identity);
-
-        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
-
-        // spawn the bullet on the clients
-        NetworkServer.Spawn(bullet);
-
-        // when the bullet is destroyed on the server it will automaticaly be destroyed on clients
-        Destroy(bullet, 5.0f);
-    }
-
+    // Update is called once per frame
     void Update()
     {
-        if (!isLocalPlayer)
+        if (isLocalPlayer)
         {
-            return;
+            SavePos();
+            // run function
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            CmdFire();
-        }
-
-        //var x = Input.GetAxis("Horizontal") * 0.1f;
-        //var z = Input.GetAxis("Vertical") * 0.1f;
-
-        //transform.Translate(x, 0, z);
-        transform.position = Vector3.Lerp(transform.position, Target.transform.position, 0.5f);
     }
 
-    void Fire()
+
+    void SavePos()
     {
-        var bullet = (GameObject)Instantiate(
-           bulletPrefab,
-           transform.position - transform.forward,
-           Quaternion.identity);
-        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
-        Destroy(bullet, 5.0f);
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                transform.position = hit.point;
+            }
+
+        }
     }
 }
