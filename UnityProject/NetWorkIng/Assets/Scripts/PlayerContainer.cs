d7b67cd82b748e0e99c4fec1feb127bf9a9b93d6
Thomacia <thomas@thoemda.com>
2016-06-09 11:15:26 +0200

    no message

--- UnityProject/NetWorkIng/Assets/Scripts/PlayerContainer.cs
+++ UnityProject/NetWorkIng/Assets/Scripts/PlayerContainer.cs
@@ -8,9 +8,30 @@ public class PlayerContainer : NetworkBehaviour
     [SyncVar] public int teamSide;
     [SyncVar] public int tankNumber;
 
+    [SerializeField]
+    private GameObject[] _tankArray;
+
+    [SerializeField]
+    private GameObject[] _spawnPoints;
 
     void Start()
     {
+        switch (tankNumber)
+        {
+            case 0:
+                Network.Instantiate(_tankArray[tankNumber], _spawnPoints[teamSide].transform.position, (new Quaternion(0, 0, 0, 0)),0);
+                Debug.Log("Spawn 0");
+                break;
+            case 1:
+                Network.Instantiate(_tankArray[tankNumber], _spawnPoints[teamSide].transform.position, (new Quaternion(0, 0, 0, 0)), 0);
+                Debug.Log("Spawn 1");
+                break;
+
+            case 2:
+                Network.Instantiate(_tankArray[tankNumber], _spawnPoints[teamSide].transform.position, (new Quaternion(0, 0, 0, 0)), 0);
+                Debug.Log("Spawn 2");
+                break;
+        }
         Debug.Log(teamSide + tankNumber);
     }
 	
